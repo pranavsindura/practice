87f89334b96080a035145bda8fce42689b8901c6
Pranav Sindura <pranav.sindura@gmail.com>
2018-09-10 19:46:49 +0530

    Fourth COmmit

--- test.js
+++ test.js
@@ -1,27 +1,17 @@
-let grades = [34,13,65,78,77,98,43,81];
+let grades = [34, 13, 65, 78, 77, 98, 43, 81];
 
 let newGrades = [];
-    
-    grades.forEach((e)=>{
-        if(e<38)
-            {
-                newGrades.push(e);
-            }
-        else
-            {
-                /*
-                43, 43%5=3 43/5=8
-                */
-                let next = (Math.trunc(e/5)+1)*5;
-                if(next - e < 3)
-                    newGrades.push(next);
-                else
-                {
-                    newGrades.push(e);
-                }
-                
-            }
-        
-    });
 
-    console.log(newGrades);
+grades.forEach(e => {
+  if (e < 38) {
+    newGrades.push(e);
+  } else {
+    let next = (Math.trunc(e / 5) + 1) * 5;
+    if (next - e < 3) newGrades.push(next);
+    else {
+      newGrades.push(e);
+    }
+  }
+});
+
+console.log(newGrades);
